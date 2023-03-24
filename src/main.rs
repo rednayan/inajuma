cc93805a77d03d4d0bee4a893bc6344e636a389e
nayan <nayansharma1010@gmail.com>
2023-03-24 13:51:37 +0530

    fix: vga buffer issue with qemu

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@
 use core::panic::PanicInfo;
 
 
-static HELLO: &[u8] = b"BUFFER!";
-#[no_mangle] 
-pub extern "C" fn _start() -> ! { //linker looks for the entry point named '_start' by default
+static HELLO: &[u8] = b"inajumax86_64!";
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i,&byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(1 as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2+1) = 0xb;
         }
     }
     loop {}
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { //called on panic
+fn panic(_info: &PanicInfo) -> ! { 
     loop{}
 }
 
