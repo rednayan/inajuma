f0343dd5f6c57091cd67dc32d4633048409c3f9e
nayan <nayansharma1010@gmail.com>
2023-03-24 16:49:05 +0530

    feat: memory mapping vga text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
-#![no_std] //removing link for rust standart library
-#![no_main] //remove all entry points (Rust level)
-use core::panic::PanicInfo;
+#![no_std]
+#![no_main]
+use core::{panic::PanicInfo};
 
+mod vga_buffer;
 
-static HELLO: &[u8] = b"inajumax86_64!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i,&byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2+1) = 0xb;
-        }
-    }
-    loop {}
+    println!("inajuma os");
+    panic!("some line did panic");
+    loop {};
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! { 
-    loop{}
+fn panic(_info: &PanicInfo) -> ! {
+    print!("{}",_info);    
+   loop{};
 }
-
