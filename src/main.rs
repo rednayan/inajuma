f73e247debe52ecdbfb9480b31955bb9339c4eda
nayan <nayansharma1010@gmail.com>
2023-03-25 12:11:22 +0530

    feat: test trivial assertion

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use core::{panic::PanicInfo};
 
 mod vga_buffer;
 
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("inajuma os");
-    panic!("some line did panic");
+    // panic!("some line did panic");
+    #[cfg(test)]
+    test_main();
     loop {};
 }
 
@@ -15,4 +21,19 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     print!("{}",_info);    
    loop{};
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1,1);
+    println!("[ok]");
 }
