c4b53f56485176c27ef30f5c00662ee2d4fba560
nayan <nayansharma1010@gmail.com>
2023-03-27 18:20:35 +0530

    feat: initialize def excpt...

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use crate::println;
 use lazy_static::lazy_static;
@@ -6,6 +8,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -19,6 +22,10 @@ extern "x86-interrupt" fn breakpoint_handler(
 )
 {
     println!("EXCEPTION: BREAKPOINT\n{:#?}",stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame,_error_code: u64) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 #[test_case]
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,9 @@ pub extern "C" fn _start() -> ! {
     println!("Welcome to the inajuma os 1.0");
     inajuma::init();
 
-    //invoke a breakpoing exception
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    }
 
     #[cfg(test)]
     test_main();
