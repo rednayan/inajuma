2255bb52ac311f072be86a6da29955378c0da57f
nayan <nayansharma1010@gmail.com>
2023-03-29 18:27:27 +0530

    feat: created GDT ...

--- src/gdt.rs
+++ src/gdt.rs
@@ -3,6 +3,7 @@ use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
 
 lazy_static! {
     static ref TSS: TaskStateSegment = {
@@ -21,3 +22,16 @@ lazy_static! {
         tss
     };
 }
+
+lazy_static! {
+    static ref GDT:GlobalDescriptorTable = {
+        let mut gdt = GlobalDescriptorTable::new();
+        gdt.add_entry(Descriptor::kernel_code_segment());
+        gdt.add_entry(Descriptor::tss_segment(&TSS));
+        gdt
+    };
+ }
+
+ pub fn init() {
+    GDT.load();
+ }
--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,14 @@
 #![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
+
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
 
