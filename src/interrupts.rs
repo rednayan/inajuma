5b24677ce1d6a1e12622d07ef75a4e706a92df99
nayan <nayansharma1010@gmail.com>
2023-03-29 18:51:53 +0530

    feat: segmentation -- arch..

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,7 @@
 use core::panic;
 
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::gdt;
 use crate::println;
 use lazy_static::lazy_static;
 
@@ -8,7 +9,11 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+            
         idt
     };
 }
